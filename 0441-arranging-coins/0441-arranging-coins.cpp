9e12cd5f8a5235b6a7d21260ce02a303882477c1
Nishil <128979710+nishil-3@users.noreply.github.com>
2024-08-06 14:49:42 +0530

    Time: 6 ms (43.36%), Space: 8.6 MB (7.15%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int num = n ;
+        // using binary search
+
+        int low = 1;
+        int high = n;
         int ans = 0;
-        int cnt = 0;
+        long long mid=low+(high-low)/2;
+        while (low <= high) {
 
-        for (int i =1;i<=n;i++)
-        {
-            if (num-i>=0)
-            {
-                ans = num-i;
-                cnt++;
+            long long sumo = mid * (mid + 1) / 2;
+            if (sumo <= n) {
+                ans = mid;
+                low = mid + 1;
+            } else if (sumo > n) {
+                high = mid - 1;
             }
-            else{
-              
-                break ; 
-            }
-            num = ans ; 
-
+            mid=low+(high-low)/2;
         }
-        return cnt ; 
+        return ans;
     }
 };
