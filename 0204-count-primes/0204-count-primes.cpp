15395223a71137b53674b7e2c0b45aa6d39e5759
Nishil <128979710+nishil-3@users.noreply.github.com>
2024-10-04 02:39:01 +0530

    Time: 137 ms (84.75%), Space: 29.4 MB (24.91%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        // using Sieve of Eratosthenes
+        int arr[n+1] ;
+        for(int i =0;i<n;i++)
+        {
+            arr[i]=1;
+        }
         int cnt = 0;
-        vector<bool> prime(n + 1, true);
-        prime[0] = prime[1] = false;
-        for (int i = 2; i < n; i++) {
-            if (prime[i]) {
+        for(long long i=2;i<=n;i++)
+        {
+            if (arr[i]==1)
+            {
                 cnt++;
-                for (int j = i * 2; j < n; j = j + i) {
-                    prime[j] = 0;
+
+                for (long long val = i*i;val<=n;val+=i)
+                {
+                    arr[val]= 0;
                 }
             }
         }
-        return cnt;
+        return cnt ;
+        
     }
 };
