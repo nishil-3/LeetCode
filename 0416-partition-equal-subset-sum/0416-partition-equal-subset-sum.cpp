9fa1681bd58092ea595cb5c6bc35758cb7b89535
Nishil <128979710+nishil-3@users.noreply.github.com>
2025-07-17 22:17:52 +0530

    Time: 141 ms (42.85%), Space: 110.4 MB (39.84%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-bool ans (vector<int> &nums, int index, int sum,  vector<vector<int>> &dp)
-{
-    if (sum == 0) return true;
-        if (index == 0) return nums[0] == sum;  // Fix base condition
-
-        if(dp[index][sum]!=-1) return dp[index][sum];
-
-
-        bool notPick = ans(nums, index - 1, sum,dp);
+    bool check(vector<int>& nums, int index, int target,vector<vector<int>>& dp) {
+        if (target == 0)
+            return true;
+        if (index == 0)
+            return nums[0] == target;
+        // pick and not pick
+        if(dp[index][target]!=-1) return dp[index][target];
+        bool notpick = check(nums, index - 1, target,dp);
         bool pick = false;
-        if (sum >= nums[index]) {
-            pick = ans(nums, index - 1, sum - nums[index],dp);
+        if (target >= nums[index]) {
+            pick = check(nums, index - 1, target - nums[index],dp);
         }
-        return dp[index][sum] = notPick || pick;
-
-
-}
+        return dp[index][target] = pick || notpick;
+    }
     bool canPartition(vector<int>& nums) {
-         int n = nums.size();
+        int n = nums.size();
         int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-        }
-        if (sum % 2 == 1) return false;
-
-        int k = sum / 2;
-        vector<vector<int>>dp(n,vector<int>(k+1,-1));
-        return ans(nums, n - 1, k,dp);
-
-        
+        for (auto it : nums)
+            sum += it;
+        if (sum % 2 == 1)
+            return false;
+            int k = sum/2;
+            vector<vector<int>> dp(n,vector<int>(k+1,-1));
+        return check(nums, n - 1, k,dp);
     }
 };
