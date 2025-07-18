1a2a530592ea93a0d5eaa5351a67a82f7cebc8c7
Nishil <128979710+nishil-3@users.noreply.github.com>
2025-07-19 00:00:58 +0530

    Time: 147 ms (39.04%), Space: 17.2 MB (70.5%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    bool check(vector<int>& nums, int index, int target,vector<vector<int>>& dp) {
-        if (target == 0)
-            return true;
-        if (index == 0)
-            return nums[0] == target;
-        // pick and not pick
-        if(dp[index][target]!=-1) return dp[index][target];
-        bool notpick = check(nums, index - 1, target,dp);
-        bool pick = false;
-        if (target >= nums[index]) {
-            pick = check(nums, index - 1, target - nums[index],dp);
-        }
-        return dp[index][target] = pick || notpick;
+    bool check(vector<int>& nums, int n, int target,vector<vector<bool>>& dp) {
+        // base case 
+          for (int i = 0; i <= n; i++) dp[i][0] = true;
+    for (int j = 1; j <= target; j++) dp[0][j] = false;
+        for(int i =1;i<=n;i++)
+         {
+            for(int j =1;j<=target;j++)
+            {
+                if (i==0 || j==0) dp[i][j]=false;
+                else if(nums[i-1]>j)
+                {
+                    // not pick 
+                    dp[i][j]= dp[i-1][j];
+                }
+                else if (j>=nums[i-1])
+                {
+                    dp[i][j]=dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                }
+            }
+         }
+         return dp[n][target];
     }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
@@ -22,7 +30,7 @@ public:
         if (sum % 2 == 1)
             return false;
             int k = sum/2;
-            vector<vector<int>> dp(n,vector<int>(k+1,-1));
-        return check(nums, n - 1, k,dp);
+            vector<vector<bool>> dp(n+1,vector<bool>(k+1,false));
+        return check(nums, n , k,dp);
     }
 };
